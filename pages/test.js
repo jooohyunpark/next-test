4999663aaa317a4c8c2157b1cf0bd0b9ab66626b
Joohyun Park <jhp527@nyu.edu>
2021-02-02 20:42:08 -0500

    init

---
+++ pages/test.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+    <div>ddd</div>
+      </main>
+
+      <footer >
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo"  />
+        </a>
+      </footer>
+    </div>
+  )
+}
